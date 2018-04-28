44722a1ef1cb67bbf65c7a93b534d86e27afeb06
shakedzrihen <shaked.zrihen@gmail.com>
2018-04-28 17:48:12 +0300

    Add main to check the code

--- CheckList/main.cpp
+++ CheckList/main.cpp
@@ -1,4 +1,16 @@
-//
-// Created by Shaked Zrihen on 28/04/2018.
-//
+#include "CheckList.h"
 
+
+int main (int argc, char *argv[]) {
+
+    string test[] = { "Ligal Levy", "Chen Keselman", "Avraham Lachmi", "Shaked Zrihen" };
+    CheckList list(4, test, 4, 4);
+    list.Draw();
+    HANDLE in = GetStdHandle(STD_INPUT_HANDLE);
+    while (1) {
+        INPUT_RECORD inputRecord;
+        DWORD count;
+        ReadConsoleInput(in, &inputRecord, 1, &count);
+        list.HandleInputRecord(inputRecord);
+    }
+}
