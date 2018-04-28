759e597542fabadb035146ef796925fd02d72295
shakedzrihen <shaked.zrihen@gmail.com>
2018-04-28 14:04:29 +0300

    Add attribute to class ItemList

--- CheckList/ListItem.h
+++ CheckList/ListItem.h
@@ -14,20 +14,31 @@ using namespace std;
 class ListItem {
 
     private:
+        // Atrributes
         HANDLE _stdoutHandle;
         COORD _position;
         string _descriptionLabel;
         short _lineLengh;
+        bool _isChecked;
+        bool _isFocusd;
+
+        // Methods
+        void markAsChoose();
+        void markAsUnchoose();
+        bool setColor(DWORD color);
 
 
 
-    public:
+public:
         // C'tors
         ListItem(short positionX, short positionY, string description);
-
-        // functions
+        // methods
         void draw();
         bool setBackgroundColor(DWORD background);
+        bool setTextColor(DWORD textColor);
+        void click();
+        bool focus();
+        bool unfocus();
 
         //D'tors
         ~ListItem();
