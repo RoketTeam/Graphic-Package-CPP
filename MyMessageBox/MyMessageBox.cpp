fed96b5a42a514b5422b25af2cf1772f38a17dbe
shaked zrihen <shaked.zrihen@gmail.com>
2018-06-29 23:27:06 +0300

    Refactor messageBox drawer & action functions

--- MyMessageBox/MyMessageBox.cpp
+++ MyMessageBox/MyMessageBox.cpp
@@ -11,11 +11,13 @@ void MyMessageBox::draw(Graphics& g, int x, int y, size_t z){
             g.setBackground(background_);
         if(foreground_ != Color::Transparent)
             g.setForeground(foreground_);
+        width_ = ok_.getWidth() + cancel_.getWidth() + message_.getText().length();
         if(border_)
-            border_->drawBorder(left_, top_, width_, g, getHeight());
-        message_.draw(g, x+1, y+1, 0);
-        ok_.draw(g, x+2, y+3, 0);
-        cancel_.draw(g, x+10, y+3, 0);
+            border_->drawBorder(x, y, width_, g, getHeight());
+        int center = width_ / 4 - 1;
+        message_.draw(g, center, y, 0);
+        ok_.draw(g, center + 1, y+3, 0);
+        cancel_.draw(g, center + 7, y+3, 0);
 
         g.setBackground(background);
         g.setForeground(foreground);
@@ -28,12 +30,11 @@ MyMessageBox::MyMessageBox(string message) :
         message_(message)
 {
     setBorder(new OneLine());
-    setBackground(Color::Purple);
     ok_.setBackground(Color::Green);
     cancel_.setBackground(Color::Red);
 
     height_ = message_.getHeight() + 4;
-    width_ = message_.getText().length() + 4;
+    width_ = ok_.getWidth() + cancel_.getWidth() + message_.getText().length();
     is_visible = true;
     ok_.add(this);
     cancel_.add(this);
@@ -50,8 +51,12 @@ void MyMessageBox::mousePressed(int x, int y, bool isLeft) {
 
 void MyMessageBox::action(IObservable *observable) {
     // TODO: Check who is pressed and handle it
-    if(is_visible)
+    if(is_visible){
         is_visible = false;
-    else
+        ok_.setClickable(false);
+        cancel_.setClickable(false);
+    }
+    else {
         is_visible = true;
+    }
 }
