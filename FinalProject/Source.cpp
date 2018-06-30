96b4b3cfb7b72daaee35bada93aa0933b058c032
Ligal Levy <Ligalevy1@gmail.com>
2018-06-30 19:52:02 +0300

    Change driver for QA

--- FinalProject/Source.cpp
+++ FinalProject/Source.cpp
@@ -7,27 +7,29 @@
 #include "../EventEngine/EventEngine.h"
 #include "../MyMessageBox/MyMessageBox.h"
 #include "../Panel/Panel.h"
+#include "../ListItem/ListItem.h"
 
 
 int main(int argc, char** argv)
 {
-	Panel p;
-	Label* l = new Label("Im a Panel and I have: ");
-	l -> set_margin(0);
-	p.Add(l);
-
-    l = new Label("NumericBox:");
-    l -> set_margin(-1);
-    p.Add(l);
-    NumericBox* n = new NumericBox(0, 10);
-    n->set_margin(4);
-	p.Add(n);
-
-    l = new Label("MessageBox: ");
-    l -> set_margin(0);
-    p.Add(l);
-    p.Add(new MyMessageBox("I hope dsfsfsdfdsf"));
-	MyMessageBox m("lalala");
+//	Panel p;
+//	Label* l = new Label("Im a Panel and I have: ");
+//	l -> set_margin(0);
+//	p.Add(l);
+//
+//    l = new Label("NumericBox:");
+//    l -> set_margin(-1);
+//    p.Add(l);
+//    NumericBox* n = new NumericBox(0, 10);
+//    n->set_margin(4);
+//	p.Add(n);
+//
+//    l = new Label("MessageBox: ");
+//    l -> set_margin(0);
+//    p.Add(l);
+//    p.Add(new MyMessageBox("I hope dsfsfsdfdsf"));
+//	MyMessageBox m("lalala");
+    ListItem l("Messi is a LOSER");
 	EventEngine e;
-	e.run(p);
+	e.run(l);
 }
