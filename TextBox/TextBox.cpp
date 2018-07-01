1f87728726c2f3f44be5aca27223b6d92a620b58
AvrahamShenkar <37639360+AvrahamShenkar@users.noreply.github.com>
2018-07-02 01:17:15 +0300

    add keyPressed

--- TextBox/TextBox.cpp
+++ TextBox/TextBox.cpp
@@ -14,12 +14,12 @@ using namespace std;
 
 
 TextBox::TextBox() : Control(), IListener(),
-	length_(10),
-	value_("") {};
+length_(10),
+value_("") {};
 
 TextBox::TextBox(short left, short top, int length) : Control(), //IListener(),
-	length_(length),
-	value_("") 
+length_(length),
+value_("")
 {
 	setLeft(left);
 	setTop(top);
@@ -35,7 +35,7 @@ length_(length), value_(value)
 
 
 void TextBox::setLeft(short left) {
-	if (left < 0) {left = 0;}
+	if (left < 0) { left = 0; }
 	left_ = left;
 }
 
@@ -50,8 +50,8 @@ void TextBox::setFrameSize(int length) {
 
 void TextBox::addChar(int x, Graphics &g, char ch) {
 	if (length_ > value_.getText().length()) {
-		value_.setText (value_.getText() += ch);
-		g.write(this->left_+1, this->top_+1, value_.getText());
+		value_.setText(value_.getText() += ch);
+		g.write(this->left_ + 1, this->top_ + 1, value_.getText());
 	}
 }
 
@@ -83,6 +83,19 @@ void TextBox::draw(Graphics& g) {
 
 void TextBox::action(IObservable * iObservable)
 {
+}
+
+void TextBox::keyPressed(CHAR AsciiChar, Graphics& g)
+{
+	if (AsciiChar >= 32 && AsciiChar <= 127) {
+		addChar(0, g, AsciiChar);
+	}
+	else if (AsciiChar == 128) { //ir.Event.KeyEvent.uChar.AsciiChar == 0 
+		deleteAllText();
+	}
+	else if (AsciiChar == 8) {
+		deleteChar();
+	}
 }
 
 
--- TextBox/TextBox.h
+++ TextBox/TextBox.h
@@ -10,33 +10,34 @@ using namespace std;
 
 class TextBox : public Control, public IListener
 {
-	private:
-		int length_;
-		Label value_;
+private:
+	int length_;
+	Label value_;
 
-	public:
-		//C'tors
-		TextBox();
-		TextBox(short left, short top, int length);
-		TextBox(short left, short top, int length, string value );
+public:
+	//C'tors
+	TextBox();
+	TextBox(short left, short top, int length);
+	TextBox(short left, short top, int length, string value);
 
-		// methods
-		void setLeft(short left);
-		void setTop(short top);
-		void setFrameSize(int Length);
-		void addChar(int x, Graphics &g, char ch);
-		void deleteChar();
-		void deleteAllText();
-		
+	// methods
+	void setLeft(short left);
+	void setTop(short top);
+	void setFrameSize(int Length);
+	void addChar(int x, Graphics &g, char ch);
+	void deleteChar();
+	void deleteAllText();
 
-		//Handler
-		void draw(Graphics& g);
-		void action(IObservable* iObservable);
-		//void mousePressed(int x, int y, bool isLeft);
-		//bool mouseHover(int x, int y, Graphics &g);
 
-		//D'tors
-		~TextBox() {};
+	//Handler
+	void draw(Graphics& g);
+	void action(IObservable* iObservable);
+	void keyPressed(CHAR AsciiChar, Graphics& g);
+	//void mousePressed(int x, int y, bool isLeft);
+	//bool mouseHover(int x, int y, Graphics &g);
+
+	//D'tors
+	~TextBox() {};
 
 
 
