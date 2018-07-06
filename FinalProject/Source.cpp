3c3cb5c8280da8991f83beccdbccfff7f0932cbd
Shaked Zrihen <shaked.zrihen@gmail.com>
2018-07-06 19:15:45 +0300

    Update driver for regression test

--- FinalProject/Source.cpp
+++ FinalProject/Source.cpp
@@ -14,33 +14,27 @@
 
 int main(int argc, char** argv)
 {
-//	Panel p;
-//	Label* l = new Label("Im a Panel and I have: ");
-//	l -> set_margin(0);
-//	p.Add(l);
-//
-//    l = new Label("NumericBox:");
-//    l -> set_margin(-1);
-//    p.Add(l);
-//    NumericBox* n = new NumericBox(0, 10);
-//    n->set_margin(4);
-//	p.Add(n);
-//
-//    l = new Label("MessageBox: ");
-//    l -> set_margin(0);
-//    p.Add(l);
-//    CheckList radioBox;
-//    radioBox.Add(new ListItem("Ligal"));
-//    radioBox.Add(new ListItem("Chen"));
-//    radioBox.Add(new ListItem("Lahmi"));
-//    radioBox.Add(new ListItem("Shaked"));
-//    p.Add(&radioBox);
-    CheckList check_list;
-    check_list.AddSelectedItem(new ListItem("shaked"));
-    check_list.AddSelectedItem(new ListItem("Ligal"));
-    check_list.AddSelectedItem(new ListItem("Chen"));
-    check_list.AddSelectedItem(new ListItem("Avi"));
-    check_list.RemoveSelectedItem(new ListItem("Avi"));
-	EventEngine e;
-	e.run(check_list);
+	Panel p;
+	Label* l = new Label("Im a Panel and I have: ");
+	l -> set_margin(0);
+	p.Add(l);
+
+    l = new Label("NumericBox:");
+    l -> set_margin(-1);
+    p.Add(l);
+    NumericBox* n = new NumericBox(0, 10);
+    n->set_margin(4);
+	p.Add(n);
+
+    l = new Label("CheckList: ");
+    l -> set_margin(-2);
+    p.Add(l);
+    CheckList checkList;
+    checkList.AddSelectedItem(new ListItem("Ligal"));
+    checkList.AddSelectedItem(new ListItem("Chen"));
+    checkList.AddSelectedItem(new ListItem("Lahmi"));
+    checkList.AddSelectedItem(new ListItem("Shaked"));
+    p.Add(&checkList);
+    EventEngine e;
+	e.run(p);
 }
