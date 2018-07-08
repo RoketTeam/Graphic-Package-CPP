07a2b53f3e670d048006eec042db67cad3c67f9f
Chen Shavit <ck20427@gmail.com>
2018-07-08 20:42:02 +0300

    Update driver for test changes

--- FinalProject/Source.cpp
+++ FinalProject/Source.cpp
@@ -16,19 +16,34 @@ int main(int argc, char** argv)
     l = new Label("ComboBox: ");
     l -> set_margin(0);
     p.Add(l);
-//    CheckList checkList;
-//    checkList.AddSelectedItem(new ListItem("Ligal"));
-//    checkList.AddSelectedItem(new ListItem("Chen"));
-//    checkList.AddSelectedItem(new ListItem("Lahmi"));
-//    checkList.AddSelectedItem(new ListItem("Shaked"));
-//    checkList.set_margin(1);
-//    p.Add(&checkList);
     ComboBox* comboBox = new ComboBox;
     comboBox->AddSelectedItem(new ListItem("avi"));
     comboBox->AddSelectedItem(new ListItem("shaked"));
     p.Add(comboBox);
 
-    p.Add(new Label("this is a TextBos:"));
+    l = new Label("CheckList: ");
+    l -> set_margin(0);
+    p.Add(l);
+    CheckList checkList;
+    checkList.AddSelectedItem(new ListItem("Ligal"));
+    checkList.AddSelectedItem(new ListItem("Chen"));
+    checkList.AddSelectedItem(new ListItem("Lahmi"));
+    checkList.AddSelectedItem(new ListItem("Shaked"));
+    checkList.set_margin(1);
+    p.Add(&checkList);
+
+    l = new Label("RadioBox: ");
+    l -> set_margin(0);
+    p.Add(l);
+    RadioBox radioBox;
+    radioBox.AddSelectedItem(new ListItem("Dubi"));
+    radioBox.AddSelectedItem(new ListItem("Estie"));
+    radioBox.AddSelectedItem(new ListItem("Shimi"));
+    radioBox.AddSelectedItem(new ListItem("Sansa"));
+    radioBox.set_margin(1);
+    p.Add(&radioBox);
+
+    p.Add(new Label("this is a TextBox:"));
     TextBox textBox;
     textBox.set_margin(0);
     textBox.set_margin_left(0);
